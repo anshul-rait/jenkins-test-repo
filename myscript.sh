8fa0e64b38eb658def32a0390d409ee1f569ff99
anshul-rait <ans.kin.rt23@dypatil.edu>
2026-04-16 09:47:38 +0530

    Create myscript.sh

---
+++ myscript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "---------------------------------------"
+echo "SUCCESS: Jenkins has pulled the file!"
+echo "Current User: $(whoami)"
+echo "Current Date: $(date)"
+echo "---------------------------------------"
